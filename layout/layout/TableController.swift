ad9eec3e2ea8a705dad94cbaec7ce3b316301217
Fairy <fairyoo@qq.com>
2018-10-25 21:51:34 +0800

    new table view

---
+++ layout/layout/TableController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  layout
+//
+//  Created by Fairy on 2018/10/25.
+//  Copyright © 2018 fairyios. All rights reserved.
+//
+
+import UIKit
+
+class TableController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
+
+extension TableController : UITableViewDelegate {
+    
+}
+
+extension TableController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reusableCell", for: indexPath)
+        return cell
+    }
+    
+    
+}
+
