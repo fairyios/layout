9acb7d58a61ee6af604196b6ac5e35ef8cccb7cd
Fairy <fairyoo@qq.com>
2018-10-25 21:53:40 +0800

    remark

--- layout/layout/TableController.swift
+++ layout/layout/TableController.swift
@@ -21,15 +21,36 @@ class TableController: UIViewController {
 
 }
 
+
+
+// MARK: - UITableViewDelegate
 extension TableController : UITableViewDelegate {
     
 }
 
+
+// MARK: - UITableViewDataSource
 extension TableController : UITableViewDataSource {
+    
+    
+    /// 行数
+    ///
+    /// - Parameters:
+    ///   - tableView: tableView description
+    ///   - section: section description
+    /// - Returns: return value description
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
+    
+    
+    /// 重复使用单元格
+    ///
+    /// - Parameters:
+    ///   - tableView: tableView description
+    ///   - indexPath: indexPath description
+    /// - Returns: return value description
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reusableCell", for: indexPath)
         return cell
