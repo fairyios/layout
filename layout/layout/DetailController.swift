7628b595a31e12900d0c6b7e81eb8a86813deaaa
Fairy <fairyoo@qq.com>
2018-10-26 16:33:26 +0800

    DetailController
    DetailController

---
+++ layout/layout/DetailController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailController.swift
+//  layout
+//
+//  Created by Fairy on 2018/10/26.
+//  Copyright © 2018 fairyios. All rights reserved.
+//
+
+import UIKit
+
+class DetailController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
