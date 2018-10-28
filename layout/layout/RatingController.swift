11ccc4520279bf473b0a68674e3f16ddd286e27a
Fairy <fairyoo@qq.com>
2018-10-28 22:13:04 +0800

    rating
    rating

---
+++ layout/layout/RatingController.swift
@@ -0,0 +1,26 @@
+//
+//  RatingController.swift
+//  layout
+//
+//  Created by Fairy on 2018/10/28.
+//  Copyright © 2018 fairyios. All rights reserved.
+//
+
+import UIKit
+
+
+/// 评论
+class RatingController: UIViewController {
+    
+    
+    /// <#Description#>
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    /// <#Description#>
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
