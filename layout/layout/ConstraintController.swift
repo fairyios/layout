0e76619c7cf0a3af7236ba76e1bbb09be2b52b7e
Fairy <fairyoo@qq.com>
2018-10-31 17:57:17 +0800

    约束
    约束

--- layout/layout/ConstraintController.swift
+++ layout/layout/ConstraintController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-final class SpinnerController : UIViewController {
+final class ConstraintController : UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
